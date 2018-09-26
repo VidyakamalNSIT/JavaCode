43f113f30896f8dd292b844f3fb14de72459617d
Vidyakamal <31116133+VidyakamalNSIT@users.noreply.github.com>
2018-09-27 02:37:18 +0530

    Add files via upload

---
+++ StealthEdu_09/src/stealthedu_09/StealthEdu_09.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stealthedu_09;
+ 
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class StealthEdu_09 {    
+    
+    public static void main(String[] args) {
+        
+        
+    }    
+}
